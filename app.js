bb376c38d2dd0919684b7515d065db5f7c97df44
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-03 19:03:35 +0700

    feat: create pages get, post and patch routes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const pagesController = require("./src/controllers/pages.controller");
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.route("/pages").get(pagesController.read).post(pagesController.create);
+app.route("/pages/:id").patch(pagesController.update);
 
 app.use((err, _, res, __) => {
     res.json({ message: err.message });
