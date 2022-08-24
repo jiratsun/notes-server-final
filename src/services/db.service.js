1a42b92293e7fa75b6459407e2a58cba0f5527dc
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-08-24 23:46:49 +0700

    refactor: change default database

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connection = mongoose.createConnection(
-    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@master.p7hdq9h.mongodb.net/Test?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@master.p7hdq9h.mongodb.net/Guest?retryWrites=true&w=majority`,
     { useNewUrlParser: true }
 );
 
